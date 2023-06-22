2b380868e9b1a06adceab2d268b0e16a55c6a3e0
Molham Al khodari <alkhodari@drherwig.de>
2023-06-22 18:33:40 +0200

    Adding the function to create and toggle the chatbot iframe

--- script.js
+++ script.js
@@ -5,11 +5,28 @@ button.addEventListener("click", () => {
   ToggleChatbot();
 })
 
+// Initialisierung der Variablen, um das iframe-Element zu speichern
+var iframe = null;
+
 function ToggleChatbot() {
   var chatbotDivElement = document.querySelector(".chatbot");
-  if (chatbotDivElement.style.display === "none") {
-    chatbotDivElement.style.display = "block";
-  } else {
+
+  if (!iframe) {
+    // Erstellen des iframe-Elements, wenn es noch nicht existiert
+    iframe = document.createElement('iframe');
+    iframe.src = "http://127.0.0.1:8080/chatbot/";
+    iframe.width = "350px";
+    iframe.height = "500px";
+    iframe.style.border = "1px solid lightgray";
+    iframe.name = "Test";
+
+    chatbotDivElement.appendChild(iframe);
+  }
+
+  // Umschalten der Sichtbarkeit des iframe-Elements
+  if (chatbotDivElement.style.display === "block") {
     chatbotDivElement.style.display = "none";
+  } else {
+    chatbotDivElement.style.display = "block";
   }
 }
